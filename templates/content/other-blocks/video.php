305c04d68cc784901c2568d2816d5f2012a80eea
Pat Cooney <Pcooney13@github.com>
2023-09-15 08:36:21 -0400

    more files removed

--- templates/content/other-blocks/video.php
+++
@@ -1,3 +0,0 @@
-<div class="video">
-    <iframe src="<?php the_field('video_url');?>" frameborder="0"></iframe>
-</div>
--- templates/utilities/site-notices.php
+++ templates/utilities/site-notices.php
@@ -1,14 +1,24 @@
-<section class="bg-purple text-white flex p-4" id="notice-0">
-	<div class="flex items-center">
-		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-		</svg>
-		<div>Learning Management System User Guide <a class="underline transition-all duration-300 hover:text-blue hover:no-underline" target="_blank" href="http://projectteachny.org/wp-content/uploads/2020/09/PT-new-LMS-09_29_20.pdf">Click Here</a>. Please email <a class="underline transition-all duration-300 hover:text-blue hover:no-underline" href="mailto:mghcme@mgh.harvard.edu">mghcme@mgh.harvard.edu</a> with questions.</div>
+<?php
+$banner_title = get_field('title', 'options');
+$banner_text = get_field('text', 'options');
+?>
+<div x-data="{showing = true}" :class="showing=false ? 'hidden' : ''" class="flex items-center gap-x-6 bg-purple px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+	<p class="text-sm leading-6 text-white">
+		<a class="flex flex-wrap items-center" href="#">
+			<strong class="font-gotham-medium"><?php echo $banner_title; ?></strong>
+			<svg viewBox="0 0 2 2" class="hidden md:inline mx-2 h-0.5 w-0.5 fill-current" aria-hidden="true">
+				<circle cx="1" cy="1" r="1" />
+			</svg>
+			<span class="hidden md:inline font-gotham"><?php echo $banner_text; ?></span>
+			<span class="hidden md:inline font-gotham" aria-hidden="true">&rarr;</span>
+		</a>
+	</p>
+	<div class="flex flex-1 justify-end">
+		<button @on-click="showing=false" type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+			<span class="sr-only">Dismiss</span>
+			<svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+				<path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+			</svg>
+		</button>
 	</div>
-	<a class="ml-auto opacity-75 transition-all duration-300 transform hover:opacity-100 hover:scale-125" href="#" data-dismiss="notice-0" >
-		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-		</svg>
-	</a>
-</section>
-
+</div>
